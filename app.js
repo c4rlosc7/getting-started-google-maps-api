c6a45daf7b60a4ed450ebf2e375ba0a2394d440f
Carlos Andres Martinez <cmartinez@quamto.com>
2018-02-08 09:35:22 -0500

    many places geolocation and icons

--- app.js
+++ app.js
@@ -14,18 +14,55 @@ ZOOM
 var app = angular.module('App', ['ngMaterial']);
 
 app.controller('AppCtrl', function($scope, $mdDialog) {
-	
+
 	var map;
-	var location = { lat: 6.244203, lng: -75.5812119 };
+	var location = { lat: 6.244203, lng: -75.5812119, content: '<h1>Medellín</h1>' };
+	// var bogota = { lat: 4.624335, lng: -74.063644, content: '<h1>Bogota</h1>' };
+	// var pereira = {
+	// 	lat: 4.8133300,
+	// 	lng: -75.6961100,
+	// 	content: '<h1>Pereira</h1>',
+	// 	iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' };
+	// var cali = { lat: 3.4372200, lng: -76.5225000, content: '<h1>Cali</h1>' };
+	// var barranquilla = { lat: 10.9685400, lng: -74.7813200, content: '<h1>Barranquilla</h1>' };
+	var markers = [
+		{
+			lat: 6.244203,
+			lng: -75.5812119,
+			content: '<h1>Medellín</h1>'
+		},
+		{
+			lat: 4.8133300,
+			lng: -75.6961100,
+			content: '<h1>Pereira</h1>',
+			iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+		},
+		{
+			lat: 3.4372200,
+			lng: -76.5225000,
+			content: '<h1>Cali</h1>'
+		},
+		{
+			lat: 10.9685400,
+			lng: -74.7813200,
+			content: '<h1>Barranquilla</h1>'
+		}
+	];
+	/* Map Options */
+	var set = {
+		scaleControl: true,
+		zoom:5,
+		center: location
+	};
 	$scope.showMap = false;
-	$scope.showBtnMap = false;	
+	$scope.showBtnMap = false;
 
 	var getGeolocation = function(){
         /* Option */
-        var options = { 
-            maximumAge: 3000, 
-            timeout: 5000, 
-            enableHighAccuracy: true 
+        var options = {
+            maximumAge: 3000,
+            timeout: 5000,
+            enableHighAccuracy: true
         };
 
         /* Callback Success */
@@ -42,24 +79,53 @@ app.controller('AppCtrl', function($scope, $mdDialog) {
         navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 	};
 
-	//getGeolocation();	
-			
+	//getGeolocation(); scaleControl: true,
+
 	$scope.initMap = function(){
 		$scope.showMap = true;
-		$scope.showBtnMap = true;		
-		map = new google.maps.Map(document.getElementById('map'), {
-			scaleControl: true,
-		  	center: location,
-		  	zoom: 10
-		});
-
-        var infowindow = new google.maps.InfoWindow;
-        infowindow.setContent('<h2>I AM HERE!!!</h2>');
-
-        var marker = new google.maps.Marker({map: map, position: location});
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });		
+		$scope.showBtnMap = true;
+		/* New Map */
+		map = new google.maps.Map(document.getElementById('map'), set);
+		// /* Add Marker */
+    // var marker = new google.maps.Marker({
+		// 	map: map,
+		// 	position: location,
+		// 	icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+		// });
+    //
+		// var infowindow = new google.maps.InfoWindow({
+		// 	content: '<h2>I STAY HERE!!!</h2>'
+		// });
+    //
+    // marker.addListener('click', function() {
+    //   infowindow.open(map, marker);
+    // });
+		var addMarker = function(coords){
+			var marker = new google.maps.Marker({
+				position: coords,
+				map: map,
+			});
+			/* Check for customicon */
+			if(coords.iconImage) marker.setIcon(coords.iconImage);
+			/* Check content */
+			if(coords.content){
+				var infowindow = new google.maps.InfoWindow({
+					content: coords.content
+				});
+		    marker.addListener('click', function() {
+		      infowindow.open(map, marker);
+		    });
+			}
+		};
+		// addMarker(medellin);
+		// addMarker(bogota);
+		// addMarker(pereira);
+		// addMarker(cali);
+		// addMarker(barranquilla);
+		/* Loop markers */
+		for (var i = 0; i < markers.length; i++) {
+			addMarker(markers[i]);
+		}
 	};
 
 	$scope.hideMap = function(){
@@ -68,5 +134,3 @@ app.controller('AppCtrl', function($scope, $mdDialog) {
 	};
 
 });
-
-
